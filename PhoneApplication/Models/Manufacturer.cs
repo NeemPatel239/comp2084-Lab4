4f7bff015f7e54202368af3aefba59794d6708f3
Neem Patel <200429225@student.georgianc.on.ca>
2020-02-06 16:09:32 -0500

    completed lab4

---
+++ PhoneApplication/Models/Manufacturer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Manufacturer
+    {
+        public Manufacturer()
+        {
+            
+            ManufacturerName = "";
+            URL = "";
+            Founded = DateTime.Now;
+        }
+        [Key]
+        public int ManufacturerID { get; set; }
+        public String ManufacturerName { get; set; }
+        public String URL { get; set; }
+        public ICollection<Phone> PhoneList { get; set; }
+        public DateTime Founded { get; set; }
+
+    }
+}
---
+++ PhoneApplication/Models/Phone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+           
+            PhoneName = "";
+
+            DateReleased = DateTime.Now; ;
+           
+      
+        }
+        [ForeignKey("Manufacturer")]
+        public int ManufacturerID { get; set; }
+        [Key]
+        public int phoneID { get; set; }
+        public String PhoneName { get; set; }
+
+        public virtual Manufacturer Manufacturer { get; set; }
+        public int MSRP { get; set; }
+        public int ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+
+    }
+}
